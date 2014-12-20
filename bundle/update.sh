1e0750fb3334a3a9e15a8263bb4d948d8b084ff1
Chris Kolosiwsky <ckolos@gmail.com>
2014-12-20 16:32:45 -0600

    changes - removed update script - syntax clean up - borrowed ctrl-{hjkl} window movement remapping - set airline default theme to luna

--- bundle/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-for dir in $(find $HOME/.vim/bundle/ -maxdepth 1 -type d | grep -v "$HOME/.vim/bundle/$")
-  do
-      cd $dir
-      echo "Updating: $dir"
-      git pull
-      cd $HOME
-done
