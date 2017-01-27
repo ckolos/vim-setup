ecb2d356d9f4b0ada2b3be57f3bddcbe2eb6464c
Chris Kolosiwsky <ckolos@gmail.com>
2017-01-26 18:15:39 -0600

    complete hack to work around running the script from inside the vim-setup dir or outside of it

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+HERE=$PWD
 VIMDIR="$HOME/.vim"
 VIMRC="$HOME/.vimrc"
 BUNDLEDIR="$VIMDIR/bundle"
 
+if echo $HERE | grep -q vim-setup; then
+  :
+else
+  HERE="$HERE/vim-setup"
+fi
+
 cd $HOME
-test -f  "$VIMRC" && /bin/mv "${VIMRC}" "${VIMRC}.bak"
-test -f "$VIMDIR" && /bin/mv "${VIMDIR}" "${VIMDIR}.bak"
-ln -s $(dirname $0) $HOME/.vim
-ln -s $(dirname $0)/vimrc $HOME/.vimrc
+test -f  "$VIMRC" && echo /bin/mv "${VIMRC}" "${VIMRC}.bak"
+test -f "$VIMDIR" && echo /bin/mv "${VIMDIR}" "${VIMDIR}.bak"
+echo ln -s $HERE $HOME/.vim
+echo ln -s $HERE/vimrc $HOME/.vimrc
