b9ac2d77021dd087dc2c368b5178d29e7306df50
Chris Kolosiwsky <ckolos@gmail.com>
2022-05-26 20:10:26 -0500

    fix: fix install script

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+WHEREAMI="$PWD"
 CHECKOUTDIR="$(dirname "$0")"
 VIMDIR="$HOME/.vim"
 VIMRC="$HOME/.vimrc"
 
-if [[ "$CHECKOUTDIR" == "." ]]; then
+if [[ "$CHECKOUTDIR" == "." ]]; then # Running inside checkoutdir
   CHECKOUTDIR=${PWD}
+  else 
+    if [[ ! "$CHECKOUTDIR" =~ "$WHEREAMI" ]]; then # running one level up from checkdir
+      CHECKOUTDIR="$PWD/$CHECKOUTDIR"
+    fi
 fi
+
 cd "$HOME" || exit 1
 test -f  "$VIMRC" && /bin/mv "${VIMRC}" "${VIMRC}.bak"
 test -f "$VIMDIR" && /bin/mv "${VIMDIR}" "${VIMDIR}.bak"
