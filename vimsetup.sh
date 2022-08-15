a0bc35f1163bf140adf2c22993c7df962522915d
Chris Kolosiwsky <ckolos@gmail.com>
2022-08-14 20:28:44 -0500

    meh.

--- vimsetup.sh
+++ vimsetup.sh
@@ -6,8 +6,8 @@ VIMRC="$HOME/.vimrc"
 
 if [[ "$CHECKOUTDIR" == "." ]]; then # Running inside checkoutdir
   CHECKOUTDIR=${PWD}
-  else 
-    if [[ ! "$CHECKOUTDIR" =~ "$WHEREAMI" ]]; then # running one level up from checkdir
+  else
+    if [[ ! "$CHECKOUTDIR" =~ $WHEREAMI ]]; then # running one level up from checkdir
       CHECKOUTDIR="$PWD/$CHECKOUTDIR"
     fi
 fi
@@ -15,8 +15,8 @@ fi
 cd "$HOME" || exit 1
 test -f  "$VIMRC" && /bin/mv "${VIMRC}" "${VIMRC}.bak"
 test -f "$VIMDIR" && /bin/mv "${VIMDIR}" "${VIMDIR}.bak"
-ln -s "$CHECKOUTDIR" "$HOME/.vim"
-ln -s "$CHECKOUTDIR/vimrc" "$HOME/.vimrc"
+ln -s "$CHECKOUTDIR" "$VIMDIR"
+ln -s "$CHECKOUTDIR/vimrc" "$VIMRC"
 curl \
   --create-dirs \
   -fL \
